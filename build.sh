bf1eed459dadc937f96edb95e6c426b46111f2ab
Duy Nguyen <tapvanvn@gmail.com>
2021-11-29 12:16:19 +0700

    reorganize

---
+++ build.sh
@@ -0,0 +1,33 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#1 : arm64v8 | arm32v7 | buster
+
+num_arg=$#
+if [ $num_arg -ne 1 ]; then 
+    echo "Incorrect call: ./build.sh <target>"
+    echo "example: ./build.sh arm64v8"
+    exit 1
+fi
+
+target=$1
+server_url=tapvanvn
+
+pushd "$DIR"
+
+dockerfile="docker/$target.dockerfile"
+
+docker_image="ws-pubsub"
+
+if test -f "$dockerfile"; then
+
+    tag="$(<./version.txt)-$target"
+
+    docker build -t $server_url/$docker_image:$tag -f $dockerfile ./
+
+    docker push $server_url/$docker_image:$tag
+else
+    echo "The target $target is not supported"
+    exit 1
+fi
+
+popd
---
+++ docker/arm64v8.dockerfile
@@ -0,0 +1,26 @@
+FROM arm64v8/golang:1.16-alpine AS build
+
+WORKDIR /
+
+RUN apk update && apk add --no-cache git curl openssh-client gcc g++ musl-dev
+
+RUN mkdir -p /src
+
+COPY ./ /src/
+
+RUN cd /src && go get ./... && go build
+
+FROM arm64v8/alpine
+RUN apk update \
+        && apk upgrade \
+        && apk add --no-cache \
+        ca-certificates \
+        && update-ca-certificates 2>/dev/null || true
+        
+COPY --from=build               /src/go-wspubsub / 
+COPY config/config.json        /config/config.json 
+COPY config/route.json        /config/route.json 
+
+ENV PORT=8080
+
+ENTRYPOINT ["/go-wspubsub"]
